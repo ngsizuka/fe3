790836932ad8ffa3a827da16e3e9c45aff5f6282
ng_sizuka <ng.sizuka@gmail.com>
2021-06-16 13:50:05 +0800

    test

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 $(function () {
     //$("#result").load("json/edelgard.js");
-    console.log(character);
-    $.getJSON('./json/edelgard.js', function (result) {
+    //console.log(character);
+    $.getJSON('https://ngsizuka.github.io/fe3/json/edelgard.js', function (result) {
         console.log(result)
     })
+    GetJsonData();
+    console.log("get")
     function GetJsonData() {
         console.log("get")
         $.getJSON("./json/edelgard.js", function (data) {
@@ -11,6 +13,8 @@ $(function () {
         });
     }
 
+    test();
+    console.log("test")
     function test() {
         var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
         $.getJSON(flickerAPI, {
